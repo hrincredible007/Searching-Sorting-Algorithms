52dad587d345f88ccf11d3ce1da6d260f0026058
hrincredible007 <hritikbhardwaj2002@gmail.com>
2021-06-04 16:53:57 +0530

    Bubble Sort Algorithm

---
+++ BubbleSort.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("Before Sorting");
+        printArray(arr, n);
+        BubbleSorting(arr, n);
+        System.out.println("After Sorting");
+        printArray(arr, n);
+        sc.close();
+    }
+
+    private static void BubbleSorting(int[] arr, int n) {
+        for (int i = 0; i < n-1; i++) {
+            for (int j = 0; j < n-i-1; j++) {
+                if(arr[j]<arr[j+1]){
+                    int t = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = t;
+                }
+            }
+        }
+    }
+
+
+
+    private static void printArray(int[] arr, int n) {
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+}
